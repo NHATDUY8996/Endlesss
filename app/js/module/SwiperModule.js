5794c598f512e95c935eccd7f032863137befc65
quhuvan <hung.vq98@gmail.com>
2026-04-09 12:03:20 +0700

    page planning

--- app/js/module/SwiperModule.js
+++ app/js/module/SwiperModule.js
@@ -345,4 +345,32 @@ export default function SwiperModule() {
         },
         "fraction",
     );
+
+
+    functionSlider(".planbox", {
+        speed: 1200,
+        slidesPerView: 4,
+        spaceBetween: 0,
+        autoplay: {
+            delay: 8000,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 16,
+            },
+            501: {
+                slidesPerView: 2,
+                spaceBetween: 16,
+            },
+            901: {
+                slidesPerView: 3,
+                spaceBetween: 24,
+            },
+            1201: {
+                slidesPerView: 4,
+                spaceBetween: 0,
+            },
+        },
+    });
 }
